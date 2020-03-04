0599b8e6d22498953f9d0365f717af898d331d5a
Tashi Topgyal <ttopgyal@centrilogic.com>
2020-03-03 23:37:56 -0500

    new api route

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, json, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from datetime import datetime
 import os
 
 app = Flask(__name__)
@@ -39,16 +40,35 @@ class Connection():
     def __exit__(self, exc, val, traceback):
         self._conn.close()  
 
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
-@app.route('/', methods=['GET'])
+@app.route('/sales/orders/GetNewSpireOrders', methods=['GET'])
 def index():
     try:
-        db = Database()
-        result = db.query("SELECT id FROM sales_orders")
-        for row in result:
-            print("id:", row['id'])
-    except Exception as ex:
-        print(ex)
+        lastSynchronized = request.args.get('lastSynchronized')
+        
+        if not lastSynchronized:
+            raise Exception("missing lastSynchronized")
+        
+        modified = datetime.strptime(lastSynchronized, '%m/%d/%Y %H:%M:%S %p').strftime('%Y-%m-%d %H:%M:%S %p') 
+        
+        db = Database() 
 
-    return 'success'
+        result = db.query("SELECT sales_orders.id, sales_order_items.order_no AS orderNo, sales_orders.status, " + 
+                          "sales_orders.order_date AS orderDate, sales_orders._created AS created, " +
+                          "sales_orders._modified AS modified, sales_orders._modified_by AS modifiedBy, " +
+                          "sales_orders._created_by AS createdBy " + 
+                          "FROM sales_orders " + 
+                          "LEFT JOIN sales_order_items ON sales_orders.id = CAST (sales_order_items.order_no AS INTEGER) " + 
+                          "WHERE sales_orders._modified > '" + modified + "' OR " + 
+                          "sales_order_items._modified > '" + modified + "'")
+        
+        json_result = jsonify([dict(r) for r in result])
+
+    except Exception as e:
+        abort(404, description=str(e))
+
+    return json_result
 
